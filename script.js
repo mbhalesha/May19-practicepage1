150330a0da8cd02beac19435151e53bde9104290
mbhalesha <mbhalesha@gmail.com>
2018-05-19 18:40:29 -0500

    added greeting function

---
+++ script.js
@@ -0,0 +1,7 @@
+function greeting() {
+    var first = document.getElementById("firstName").value;
+    var last = document.getElementById("lastName").value;
+    var greeting = "Hi " + first + " " + last;
+
+    document.getElementById("hello").innerHTML = greeting;   
+}
